553c75401a8883989fba3c18c61bb1f16a6e67fc
nadia-win <nadiacandoit17@gmail.com>
2021-06-02 17:16:57 +0200

    Adds comments

--- code/react-vs-vanilla-js-example/react/src/App.js
+++ code/react-vs-vanilla-js-example/react/src/App.js
@@ -1,9 +1,15 @@
 import Todo from './components/Todo';
 
 function App() {
+  /* the imperative - vanilla JS way 
+  const para = document.createElement('p');
+  para.textContent = 'This is also Visible';
+  document.getElementById('root').append(para);
+  */
   return (
     <div>
       <h1>My Todos</h1>
+      <h2>this is also visible!</h2>
       <Todo text='Learn React' />
     </div>
   );
--- code/react-vs-vanilla-js-example/react/src/index.js
+++ code/react-vs-vanilla-js-example/react/src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// this does not work in vanilla js, but in react app! 
 import './index.css';
-import App from './App';
+import App from './App'; //for 3rd party library you omit .js
 
+// App holds index.html whih has root div.
+// this is the syntax from jsx. 
 ReactDOM.render(<App />, document.getElementById('root'));
